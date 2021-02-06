916f3ed9b78838a4f138167aaf5f841801cbfc2d
DXBeam <d3dx.fx@protonmail.com>
2021-02-06 22:01:06 +0100

    Version 1.1

--- Project_Avita/Assets/Scripts/Dialogue_System/Dialogue_Editor/Editor/Graph View/DialogueEditorWindow.cs
+++ Project_Avita/Assets/Scripts/Dialogue_System/Dialogue_Editor/Editor/Graph View/DialogueEditorWindow.cs
@@ -1,18 +1,118 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class DialogueEditorWindow : MonoBehaviour
+public class DialogueEditorWindow : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    private DIalogueContainerSO currentDialogueContainer;
+    private DialogueGraphView graphView;
+
+    private ToolbarMenu toolbarMenu;
+    private Label nameOfDialogueContainer;
+
+    [OnOpenAsset(1)]
+    public static bool ShowWindow(int _instanceId, int line)
+    {
+        UnityEngine.Object item = EditorUtility.InstanceIDToObject(_instanceId);
+
+        if (item is DIalogueContainerSO)
+        {
+            DialogueEditorWindow window = (DialogueEditorWindow)GetWindow(typeof(DialogueEditorWindow));
+            window.titleContent = new GUIContent("Dialogue Editor");
+            window.currentDialogueContainer = item as DIalogueContainerSO;
+            window.minSize = new Vector2(500, 250);
+            window.Load();
+        }
+
+        return false;
+    }
+
+    private void OnEnable()
+    {
+        GenerateToolBar();
+    }
+
+    private void OnDisable()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ConstructGraphView()
     {
+        // TODO: Add Graph View
+    }
+
+    private void GenerateToolBar()
+    {
+        StyleSheet styleSheet = Resources.Load<StyleSheet>("GraphViewStyleSheet");
+        rootVisualElement.styleSheets.Add(styleSheet);
+
+        Toolbar toolbar = new Toolbar();
+
+        // Save button
+        Button saveBtn = new Button()
+        {
+            text = "Save"
+        };
+        saveBtn.clicked += () =>
+        {
+            Save();
+        };
+        toolbar.Add(saveBtn);
+
+        // Load button
+        Button loadBtn = new Button()
+        {
+            text = "Load"
+        };
+        loadBtn.clicked += () =>
+        {
+            Load();
+        };
+        toolbar.Add(loadBtn);
+
+        // Dropdown menu for language
+        toolbarMenu = new ToolbarMenu();
+        foreach (LanguageType language  in (LanguageType[])Enum.GetValues(typeof(LanguageType)))
+        {
+            toolbarMenu.menu.AppendAction(language.ToString(), new Action<DropdownMenuAction>(x => Language(language, toolbarMenu)));
+        }
+        toolbar.Add(toolbarMenu);
+
+        // Name of current DialogueContainer you have open.
+        nameOfDialogueContainer = new Label("");
+        toolbar.Add(nameOfDialogueContainer);
+        nameOfDialogueContainer.AddToClassList("nameOfDialogueContainer");
+
+        rootVisualElement.Add(toolbar);
+    }
+
+    private void Load()
+    {
+        // TODO: Load it
+        Debug.Log("Load");
+        if (currentDialogueContainer != null)
+        {
+            Language(LanguageType.Russian, toolbarMenu);
+            nameOfDialogueContainer.text = "Name: " + currentDialogueContainer;
+        }
+    }
+
+    private void Save()
+    {
+        // TODO: Save it
+        Debug.Log("Save");
+    }
+
+    private void Language(LanguageType language, ToolbarMenu toolbarMenu)
+    {
+        // TODO: Language
+        toolbarMenu.text = "Language: " + language.ToString();
         
     }
 }
